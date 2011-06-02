9591f490b1f640d4e8782bbfb769c1945ac77dcf
Jamie Kirkpatrick <jkp@kirkconsulting.co.uk>
2011-06-02 10:40:51 +0100

    added: first working version of the stream handler (it's rough right now)

--- sopserve.rb
+++ sopserve.rb
@@ -2,23 +2,88 @@
 
 require 'rubygems' if RUBY_VERSION < '1.9'
 require 'bundler/setup'
-require 'sinatra/base'
+require 'sinatra'
+require 'sinatra/async'
+require 'eventmachine'
 require 'daemon_controller'
 
 
 PORT = 8908
-PID_FILE = "/tmp/sopcastd.pid"
-LOG_FILE = "sopcastd.log"
+PID_FILE = '/tmp/sopcastd.pid'
+LOG_FILE = 'sopserve.log'
+SOPCAST_LOG = 'sopcastd.log'
 TIMEOUT = 10
 THIS_DIR = File.dirname(File.expand_path(__FILE__))
 
 
+configure :development do |conf|
+  enable :logging, :dump_errors, :raise_errors
+end
+
+
+class StreamListener < EM::Connection
+  def initialize(client)
+    super
+    @client = client
+    @skipped_header = false
+  end
+
+  def post_init
+    send_data("GET / HTTP/1.1")
+  end
+
+  def receive_data(data)
+    delimiter = /[\n][\r]*[\n]/
+    if !@skipped_header and data =~ delimiter
+      @skipped_header = true
+      header, data =  data.split(delimiter)
+    end
+    if @skipped_header
+      @client.send_data(data, self)
+    end
+  end
+
+  def unbind
+    @client.close
+  end
+end
+
+
+class StreamClient
+  include EM::Deferrable
+
+  def each(&block)
+    puts "New connection"
+    @block = block
+  end
+
+  def send_data(data, listener)
+    if @block
+      @block.call(data)
+    else
+      listener.close_connection
+    end
+  end
+
+  def close
+    if @block
+      @block = nil
+      puts "Connection closed"
+      succeed
+    end
+    super
+  end
+end
+
+
 class SopServer < Sinatra::Base
+  register Sinatra::Async
+
   def initialize
     super
     @port = PORT
     @pid_file = PID_FILE
-    @log_file = LOG_FILE
+    @log_file = SOPCAST_LOG
     @timeout = TIMEOUT
   end
 
@@ -36,7 +101,7 @@ class SopServer < Sinatra::Base
     end
   end
 
-  def create_daemon(channel_id=0)
+  def daemon(channel_id=0)
     options = {
       :identifier    => 'Sopcast Daemon',
       :start_command => start_command(channel_id),
@@ -49,20 +114,28 @@ class SopServer < Sinatra::Base
     DaemonController.new(options)
   end
 
+  def set_listener(listener)
+    @listener = listener
+  end
+
   get '/status' do
-    daemon = create_daemon()
-    daemon.running? ? "running" : "not running"
+    daemon.running? ? 'running' : 'not running'
   end
 
   get '/start/:channel' do |channel|
-    daemon = create_daemon(channel)
-    daemon.start
-    redirect '/status'
+    daemon(channel).start
+    if daemon.running?
+      redirect "/stream"
+    end
   end
 
   get '/stop' do
-    daemon = create_daemon()
     daemon.stop
-    redirect '/status'
+  end
+
+  aget '/stream' do
+    client = StreamClient.new
+    EM.connect('127.0.0.1', PORT, StreamListener, client)
+    body client
   end
 end
