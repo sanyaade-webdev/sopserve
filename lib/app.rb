a521fe81f3a048916f5a23d2d4c1f904fea71ed6
Jamie Kirkpatrick <jkp@kirkconsulting.co.uk>
2011-08-29 23:31:28 +0200

    Source remote host credentials from the environment

--- lib/app.rb
+++ lib/app.rb
@@ -1,8 +1,18 @@
 # The sinatra app to handle incoming requests
 class Sopserve < Sinatra::Base
 
+  def initialize
+    @host = ENV["SOPCAST_HOST"]
+    @user = ENV["SOPCAST_USER"]
+    @pass = ENV["SOPCAST_PASS"]
+  end
+
   get "/" do
-    "ok"
+    if @host.nil? || @user.nil? || @pass.nil?
+      "Missing remote server credentials"
+    else
+      "ok"
+    end
   end
 
 end
