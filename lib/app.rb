c9ce62572b9d213d8a5420855ca0a310ec71d1cb
Jamie Kirkpatrick <jkp@kirkconsulting.co.uk>
2011-08-31 22:09:03 +0200

    Extract connection management to its own class

--- lib/app.rb
+++ lib/app.rb
@@ -1,26 +1,48 @@
+# Persistent server connection
+class ServerConnection
+  def initialize(host, user, pass, timeout=60)
+    @host, @user, @pass, @timeout  = host, user, pass, timeout
+  end
+
+  def schedule_cleanup
+    @timer |= EM.add_timer(@timeout) { self.cleanup() }
+  end
+
+  def cleanup
+    if not @session.nil? and not @session.busy?
+      @session = @session.close
+      puts "Session timed out"
+    else
+      schedule_cleanup
+    end
+  end
+
+  def session
+    raise "Missing server credentials" if @host.nil? || @user.nil? || @pass.nil?
+    @session ||= Net::SSH.start(@host, @user, :password => @pass)
+  end
+
+  def exec(cmd, output_callback)
+    schedule_cleanup
+    session.exec! cmd do |ch, stream, data|
+      puts "Executing cmd: #{cmd}"
+      output_callback.call(data) if stream == :stdout
+    end
+  end
+end
+
 
 # Sopcast channel stream
 class ChannelStream
-  def initialize(host, user, pass, channel)
-    @host = host
-    @user = user
-    @pass = pass
+  def initialize(connection, channel)
+    @connection = connection
     @channel = channel
   end
 
   def each
-    Net::SSH.start(@host, @user, :password => @pass) do |ssh|
-      channel = ssh.open_channel { |ch|
-        cmd = "sp-sc sop://broker.sopcast.com:3912/#{@channel} 3908 8908"
-        ch.exec cmd do |ch, success|
-          raise "could not execute command" unless success
-          ch.on_data { |c, data|
-            puts data
-            yield data
-          }
-        end
-      }
-    end
+    cmd = "sp-sc sop://broker.sopcast.com:3912/#{@channel} 3908 8908"
+    read_callback = Proc.new { |data| yield data }
+    @connection.exec(cmd, read_callback)
   end
 end
 
@@ -30,16 +52,12 @@ class Sopserve < Sinatra::Base
   register Sinatra::Synchrony
 
   def initialize
-    @host = ENV["SOPCAST_HOST"]
-    @user = ENV["SOPCAST_USER"]
-    @pass = ENV["SOPCAST_PASS"]
+    @connection = ServerConnection.new(ENV["SOPCAST_HOST"],
+                                       ENV["SOPCAST_USER"],
+                                       ENV["SOPCAST_PASS"])
   end
 
   get %r{/channel/([0-9]+)} do |channel|
-    if @host.nil? || @user.nil? || @pass.nil?
-      "Missing remote server credentials"
-    else
-      return ChannelStream.new(@host, @user, @pass, channel)
-    end
+    return ChannelStream.new(@connection, channel)
   end
 end
