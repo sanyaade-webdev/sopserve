e8283e878573e7e5c6bb0dc6c05f8ae019e04881
Jamie Kirkpatrick <jkp@kirkconsulting.co.uk>
2011-09-01 10:39:35 +0200

    Add port forwarding to the server connection: start and stop forwarding when opening a stream

--- lib/app.rb
+++ lib/app.rb
@@ -6,17 +6,22 @@ class ServerConnection
 
   def exec(cmd, params = {})
     status = 0
-    schedule_cleanup
-    session.open_channel { |c|
-      c.on_request("exit-status", &Proc.new{|c, data| status = data.read_long })
-      c.on_data { |c, data | params[:on_data].call(data)} if params[:on_data]
-      c.exec cmd do |c, success|
-        raise "Command failed: #{cmd}" if not success
-        params[:on_start].call() if params[:on_start]
-      end
+    on_exit = Proc.new { |c, data|
+      status = data.read_long
+      params[:on_exit].call(status) if params[:on_exit]
     }
+    open_channel(cmd, on_exit, params)
+    schedule_cleanup
     session.loop
     return status
+  end
+
+  def start_forwarding_port(localport, address, port)
+    session.forward.local(localport, address, port)
+  end
+
+  def stop_forwarding_port(localport)
+    session.forward.cancel_local(localport)
   end
 
   def cleanup
@@ -29,6 +34,23 @@ class ServerConnection
   end
 
   private
+
+  def open_channel(cmd, on_exit, params = {})
+    return session.open_channel { |channel|
+      channel.on_request("exit-status", &on_exit)
+      channel.on_data { |c, data |
+        params[:on_data].call(data) if params[:on_data]
+      }
+      channel.exec cmd do |c, success|
+        exec_failed(cmd) if not success
+        params[:on_start].call() if params[:on_start]
+      end
+    }
+  end
+
+  def exec_failed cmd
+    raise "Command failed: #{cmd}"
+  end
 
   def schedule_cleanup
     @timer |= EM.add_timer(@timeout) { self.cleanup() }
@@ -46,25 +68,37 @@ class ChannelStream
   def initialize(connection, channel)
     @connection = connection
     @channel = channel
+    @port = 8908
   end
 
   def process_started
-    puts "Started"
+    @output.call("Started")
+  end
+
+  def process_exited(status)
+    @status = status
+    @connection.stop_forwarding_port(@port)
   end
 
   def process_stdout(output)
-    puts output
+    send_data(output)
+  end
+
+  def send_data(data)
+    @output.call(data)
   end
 
   def command
-    "sp-sc sop://broker.sopcast.com:3912/#{@channel} 3908 8908"
+    "sp-sc sop://broker.sopcast.com:3912/#{@channel} 3908 #{8908}"
   end
 
   def each(&block)
-    status = @connection.exec(command,
-                              :on_start => method(:process_started),
-                              :on_data => method(:process_stdout))
-    puts "Exit status: #{status}"
+    @output = block
+    @connection.start_forwarding_port(@port, "localhost", @port)
+    @connection.exec(command,
+                     :on_start => method(:process_started),
+                     :on_exit => method(:process_exited),
+                     :on_data => method(:process_stdout))
   end
 end
 
